9104694ad0e05cb904ff764da2898d4ae5cfdc6c
Vishwanath Shinde <vishwanathshinde988@gmail.com>
2022-12-21 00:10:10 +0530

    first

--- Assignment_1/A2.c
+++ Assignment_1/A2.c
@@ -2,16 +2,16 @@
 #include<unistd.h>
 #include<sys/types.h>
 
-int main(int ac, char *av[])
+int main(int argc, char *argv[])
 {
     int pid;
     pid=fork(); // fork() can't be used in windows. It will run online and in Linux
-
+    char*args[] = {"Hello","World",NULL};
     if(pid==0)
     {
         printf("Child completed\n");
         printf("Child process id is %d\n", getpid());
-        execv("/bin/ls",av);
+        execv("./A22.exe",args);
     }
     else
     {
--- Assignment_1/A22.c
+++ Assignment_1/A22.c
@@ -1,22 +1,10 @@
-#include<stdio.h>
-#include<unistd.h>
-#include<sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
 
 int main(int ac, char *av[])
 {
-    int pid;
-    pid=fork(); // fork() can't be used in windows. It will run online and in Linux
-
-    if(pid==0)
-    {
-        printf("Child completed\n");
-        printf("Child process id is %d\n", getpid());
-        execvp("/bin/ls",av);
-    }
-    else
-    {
-        printf("This is Parent Process\n");
-        printf("Parent process id is %d\n", getpid());
-    }
+    printf("We are in A22.c\n");
+    printf("PID of A22.c : %d\n", getpid());
     return 0;
 }
